31361cf71bb70de866f8b2f4906ddbf505298597
Crimson-Hawk <warrobots.5psiml@gmail.com>
2024-03-05 20:16:34 +0800

    retry of importing hidden files

--- hiddenFiles/.ci/scripts/format/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-# SPDX-FileCopyrightText: 2019 yuzu Emulator Project
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-# Run clang-format
-cd /yuzu
-chmod a+x ./.ci/scripts/format/script.sh
-./.ci/scripts/format/script.sh
