1f7a8ed4edd2489457246bed2135342bf2e18d57
JuanCStar <petacas666@hotmail.com>
2024-03-06 13:32:29 +0100

    Reverted Copyright headers and added suyu

--- src/yuzu/about_dialog.h
+++ src/yuzu/about_dialog.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2024 suyu Emulator Project
+// SPDX-FileCopyrightText: Copyright 2018 yuzu Emulator Project & 2024 suyu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 // Modified by JuanCStar on 2024/03/06
